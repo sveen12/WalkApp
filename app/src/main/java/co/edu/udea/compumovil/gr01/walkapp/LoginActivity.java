cf20bd3017cffa46fb9294a9fee93257bd408a63
Richard Morales <sveen12@gmail.com>
2016-08-31 08:31:54 -0500

    Longin Facebook (Hash Key error) e inicio Maps

---
+++ app/src/main/java/co/edu/udea/compumovil/gr01/walkapp/LoginActivity.java
@@ -0,0 +1,212 @@
+package co.edu.udea.compumovil.gr01.walkapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private ProgressDialog progressDialog;
+    private EditText et_user;
+    private EditText et_password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        et_user = (EditText) findViewById(R.id.et_user);
+        et_password = (EditText) findViewById(R.id.et_password);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        callbackManager =  CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                System.out.println("onSuccess");
+                progressDialog = new ProgressDialog(LoginActivity.this);
+                progressDialog.setMessage("Procesando datos...");
+                progressDialog.show();
+                String accessToken = loginResult.getAccessToken().getToken();
+                Log.i("accessToken", accessToken);
+
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.i("LoginActivity", response.toString());
+                        // Get facebook data from login
+                        Bundle bFacebookData = getFacebookData(object);
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id, first_name, last_name, email,gender, birthday, location"); // Parámetros que pedimos a facebook
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+                //requestUserProfile(loginResult);
+                goMainScreen();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), R.string.cancel_login, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(), R.string.error_login, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void loginFacebook(View view){
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email"));
+    }
+
+    private Bundle getFacebookData(JSONObject object) {
+
+        try {
+            Bundle bundle = new Bundle();
+            String id = object.getString("id");
+
+            try {
+                URL profile_pic = new URL("https://graph.facebook.com/" + id + "/picture?width=200&height=150");
+                Log.i("profile_pic", profile_pic + "");
+                bundle.putString("profile_pic", profile_pic.toString());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            bundle.putString("idFacebook", id);
+            if (object.has("first_name"))
+                bundle.putString("first_name", object.getString("first_name"));
+            if (object.has("last_name"))
+                bundle.putString("last_name", object.getString("last_name"));
+            if (object.has("email"))
+                bundle.putString("email", object.getString("email"));
+            if (object.has("gender"))
+                bundle.putString("gender", object.getString("gender"));
+            if (object.has("birthday"))
+                bundle.putString("birthday", object.getString("birthday"));
+            if (object.has("location"))
+                bundle.putString("location", object.getJSONObject("location").getString("name"));
+
+            return bundle;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void normalLogin(View view) {
+
+        if(et_user.getText().toString().equals("")){
+            if(et_password.getText().toString().equals("")){
+                et_user.requestFocus();
+                et_user.setError(getString(R.string.errorUser));
+                et_password.setError(getString(R.string.errorPassword));
+                mensajeError();
+                return;
+            }else{
+                et_user.requestFocus();
+                et_user.setError(getString(R.string.errorUser));
+                mensajeError();
+                return;
+            }
+        }else if(et_password.getText().toString().equals("")){
+            et_password.requestFocus();
+            et_password.setError(getString(R.string.errorPassword));
+            mensajeError();
+            return;
+        }
+
+        if(et_user.getText().toString().equals("admin") && et_password.getText().toString().equals("admin")){
+            goMainScreen();
+        }else{
+            Toast.makeText(this, R.string.wrongUserPass, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void mensajeError(){
+        Toast.makeText(this, R.string.emptyFields, Toast.LENGTH_LONG).show();
+    }
+
+    public void createAccount(View view) {
+    }
+
+    /*//-Para extraer los datos
+    public void requestUserProfile(LoginResult loginResult){
+        GraphRequest.newMeRequest(
+                loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+
+                    public void onCompleted(JSONObject me, GraphResponse response) {
+                        if (response.getError() != null) {
+                            // handle error
+                        } else {
+                            try {
+                                String email = response.getJSONObject().get("email").toString();
+                                Log.e("Result", email);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            String id = me.optString("id");
+
+                            // send email and id to your web server
+                            Log.e("Result1", response.getRawResponse());
+                            Log.e("Result", me.toString());
+                        }
+                    }
+                }).executeAsync();
+    }
+    */
+
+
+}
